07f891b14c5a0c9590be7c89c3602c1086e8423b
val <oppatm7@gmail.com>
2025-12-27 20:28:19 +0700

    test be 3

--- backend/utils/FirebaseAdmin.js
+++ backend/utils/FirebaseAdmin.js
@@ -1,19 +1,12 @@
 import admin from "firebase-admin";
 import 'dotenv/config';
 
-if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
-  throw new Error("FIREBASE_SERVICE_ACCOUNT is missing");
-}
-
-const serviceAccount = JSON.parse(
-  Buffer.from(
-    process.env.FIREBASE_SERVICE_ACCOUNT,
-    "base64"
-  ).toString("utf8")
-);
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(({
+    projectId: process.env.PROJECT_ID,
+    privateKey: process.env.PRIVATE_KEY,
+    clientEmail: process.env.CLIENT_EMAIL,
+  }))
 });
 
 export default admin
