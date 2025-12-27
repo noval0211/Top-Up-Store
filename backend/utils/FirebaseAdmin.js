0cbfb10d8f4fedf3ddbfb051521e89164adf62ed
val <oppatm7@gmail.com>
2025-12-27 20:43:07 +0700

    test firebase be

--- backend/utils/FirebaseAdmin.js
+++ backend/utils/FirebaseAdmin.js
@@ -4,7 +4,7 @@ import 'dotenv/config';
 admin.initializeApp({
   credential: admin.credential.cert(({
     projectId: process.env.PROJECT_ID,
-    privateKey: process.env.PRIVATE_KEY,
+    privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
     clientEmail: process.env.CLIENT_EMAIL,
   }))
 });
