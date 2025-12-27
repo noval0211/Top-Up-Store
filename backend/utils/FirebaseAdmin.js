7a2c8ef9f09991488b2485ae569aea8bece43771
val <oppatm7@gmail.com>
2025-12-27 20:46:42 +0700

    test firebase be

--- backend/utils/FirebaseAdmin.js
+++ backend/utils/FirebaseAdmin.js
@@ -1,12 +1,18 @@
 import admin from "firebase-admin";
 import 'dotenv/config';
 
-admin.initializeApp({
-  credential: admin.credential.cert(({
-    projectId: process.env.PROJECT_ID,
-    privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-    clientEmail: process.env.CLIENT_EMAIL,
-  }))
-});
+if (!FIREBASE_PRIVATE_KEY) {
+  throw new Error("FIREBASE_PRIVATE_KEY is missing")
+}
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(({
+      projectId: process.env.PROJECT_ID,
+      privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+      clientEmail: process.env.CLIENT_EMAIL,
+    }))
+  });
+}
 
 export default admin
